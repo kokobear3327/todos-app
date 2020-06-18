5e03b44774d6360a92d34429d443161a901d0b17
RichardPitts1 <Richardepitts@gmail.com>
2020-06-18 11:34:20 -0500

    -

--- database/migrations/2020_06_18_135617_create_todos_table_two.php
+++ database/migrations/2020_06_18_135617_create_todos_table_two.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTodosTable extends Migration
+class CreateTodosTableTwo extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTodosTable extends Migration
      */
     public function up()
     {
-        Schema::create('todos', function (Blueprint $table) {
+        Schema::create('todos_table_two', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateTodosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('todos');
+        Schema::dropIfExists('todos_table_two');
     }
 }
